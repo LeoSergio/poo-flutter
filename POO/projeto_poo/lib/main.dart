5d84bfdfe5264798e09862fb063e0405103a178a
LeoSergio <leandrooos222@gmail.com>
2026-03-17 19:01:54 -0300

    receita2: passo1 ao 6. git commit -m receita2:passo1-6

--- POO/projeto_poo/lib/main.dart
+++ POO/projeto_poo/lib/main.dart
@@ -1,87 +1,51 @@
 import 'package:flutter/material.dart';
 
+class CaixaDeIconesQueEuMesmoProgramei extends StatelessWidget{
+  CaixaDeIconesQueEuMesmoProgramei();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: const [
+      BottomNavigationBarItem(
+        label: "Cafés",
+        icon: Icon(Icons.coffee_outlined),
+
+      ),
+      BottomNavigationBarItem(
+          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+
+      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
+
+    ]);
+
+  }
+
+}
+
+
+
+
 void main() {
   MaterialApp app = MaterialApp(
-    theme: ThemeData(
-      primarySwatch: Colors.deepPurple,
-    ),
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Filmes..."),
-      ),
-      
-      // O corpo do app com a nossa lista expansível
-      body: ListView(
-        children: const [
-          ExpansionTile(
-            title: Text("O Poderoso Chefão", style: TextStyle(fontSize: 18)),
-            subtitle: Text("Crime/Drama"),
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-            ],
+
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Dicas")),
+        body: Column(children: [
+          Expanded(
+            child: Text("La Fin Du Monde - Bock - 65 ibu"),
           ),
-          ExpansionTile(
-            title: Text("Interestelar", style: TextStyle(fontSize: 18)),
-            subtitle: Text("Ficção Científica"),
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in odio in velit iaculis sollicitudin.",
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-            ],
+          Expanded(
+            child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
           ),
-          ExpansionTile(
-            title: Text("Pulp Fiction", style: TextStyle(fontSize: 18)),
-            subtitle: Text("Crime"),
-            children: [
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.",
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-      
-      // Trazendo de volta a barra inferior!
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0), // Um respiro extra
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.movie), 
-              iconSize: 30, 
-              color: Colors.deepPurple, // Dando uma cor para mostrar que está "ativo"
-              onPressed: () => print("Aba Filmes"),
-            ),
-            IconButton(
-              icon: const Icon(Icons.search), 
-              iconSize: 30, 
-              onPressed: () => print("Aba Busca"),
-            ),
-            IconButton(
-              icon: const Icon(Icons.settings), 
-              iconSize: 30, 
-              onPressed: () => print("Aba Configurações"),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
+          Expanded(
+            child: Text("Duvel - Pilsner - 82 ibu"),
+          )
+        ]),
+        bottomNavigationBar: CaixaDeIconesQueEuMesmoProgramei(),
+      ));
 
   runApp(app);
+
 }
