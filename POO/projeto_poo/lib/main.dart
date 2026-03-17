edacedab720df85fece2634c4f7ebd7559304da7
LeoSergio <leandrooos222@gmail.com>
2026-03-17 19:30:44 -0300

    questão1: create class ListaDeBebidas

--- POO/projeto_poo/lib/main.dart
+++ POO/projeto_poo/lib/main.dart
@@ -1,55 +1,71 @@
 import 'package:flutter/material.dart';
+
+// --- Nossa nova "caixa" para a barra de navegação ---
 class NewNavBar extends StatelessWidget {
   NewNavBar();
+
   void botaoFoiTocado(int index) {
-
     print("Tocaram no botão $index");
-
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
-      BottomNavigationBarItem(
-        label: "Cafés",
-        icon: Icon(Icons.coffee_outlined),
-
-      ),
-      BottomNavigationBarItem(
-          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-
-      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
-
-    ]);
-
+    return BottomNavigationBar(
+      onTap: botaoFoiTocado, 
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+          icon: Icon(Icons.coffee_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: "Cervejas", 
+          icon: Icon(Icons.local_drink_outlined)
+        ),
+        BottomNavigationBarItem(
+          label: "Nações", 
+          icon: Icon(Icons.flag_outlined)
+        )
+      ]
+    );
   }
-
 }
 
+// --- Nossa nova "caixa" para o corpo do aplicativo ---
+class ListaDeCervejas extends StatelessWidget {
+  // Construtor
+  ListaDeCervejas();
 
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Expanded(
+          child: Text("La Fin Du Monde - Bock - 65 ibu"),
+        ),
+        Expanded(
+          child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
+        ),
+        Expanded(
+          child: Text("Duvel - Pilsner - 82 ibu"),
+        )
+      ]
+    );
+  }
+}
 
-
+// --- A função principal agora fica super limpa! ---
 void main() {
   MaterialApp app = MaterialApp(
-
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: Scaffold(
-        appBar: AppBar(title: Text("Dicas")),
-        body: Column(children: [
-          Expanded(
-            child: Text("La Fin Du Monde - Bock - 65 ibu"),
-          ),
-          Expanded(
-            child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
-          ),
-          Expanded(
-            child: Text("Duvel - Pilsner - 82 ibu"),
-          )
-        ]),
-        bottomNavigationBar: NewNavBar(),
-      ));
+    theme: ThemeData(primarySwatch: Colors.deepPurple),
+    home: Scaffold(
+      appBar: AppBar(title: const Text("Dicas")),
+      
+      // Olha como fica elegante chamar as nossas próprias classes:
+      body: ListaDeCervejas(), 
+      bottomNavigationBar: NewNavBar(),
+      
+    )
+  );
 
   runApp(app);
-
 }
