769a66c712621fa33474263ea3bedf955999aae9
LeoSergio <leandrooos222@gmail.com>
2026-03-25 15:56:31 -0300

    add passo 3

---
+++ POO/projeto_poo/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Bebidas")),
+
+        body: DataBodyWidget(),
+
+        bottomNavigationBar: NewNavBar(),
+      ),
+    );
+  }
+}
+
+class NewNavBar extends StatelessWidget {
+  NewNavBar();
+
+  void botaoFoiTocado(int index) {
+    print("Tocaram no botão $index");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: botaoFoiTocado,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+
+          icon: Icon(Icons.coffee_outlined),
+        ),
+
+        BottomNavigationBarItem(
+          label: "Cervejas",
+          icon: Icon(Icons.local_drink_outlined),
+        ),
+
+        BottomNavigationBarItem(
+          label: "Nações",
+          icon: Icon(Icons.flag_outlined),
+        ),
+      ],
+    );
+  }
+}
+
+class DataBodyWidget extends StatelessWidget {
+  List<String> objects;
+
+  DataBodyWidget({this.objects = const []}); //model boxe
+
+  @override
+  Widget build(BuildContext context) {
+    List <Expanded> allTheLines = [];
+
+    for (var obj in objects){
+      allTheLines.add(
+        Expanded(
+          child: Center (child: Text(obj)),
+
+        )
+      );
+      
+    }
+    return Column(children: allTheLines);
+} 
+
+void main() {
+  MyApp app = MyApp();
+
+  runApp(app);
+}
