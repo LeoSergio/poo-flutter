900308abbbd0c9273d2a39e8eaba9decc97f017a
LeoSergio <leandrooos222@gmail.com>
2026-04-13 19:10:30 -0300

    initial commit receita4, p1 e p2

--- POO/projeto_poo/lib/main.dart
+++ POO/projeto_poo/lib/main.dart
@@ -1,45 +1,17 @@
 import 'package:flutter/material.dart';
 
-// Classe simples para estruturar as opções de tema do menu
-class ThemeOption {
-  final String name;
-  final MaterialColor swatch;
+var dataObjects = [
+  "La Fin Du Monde - Bock - 65 ibu",
 
-  ThemeOption({required this.name, required this.swatch});
-}
+  "Sapporo Premiume - Sour Ale - 54 ibu",
 
-// Nossa nova AppBar customizada estendendo a AppBar padrão
-class MyAppBar extends AppBar {
-  // Lista de opções de cores transformada em estática para ser acessada no construtor
-  static final List<ThemeOption> menuCores = [
-    ThemeOption(name: 'Roxo Profundo (Padrão)', swatch: Colors.deepPurple),
-    ThemeOption(name: 'Vermelho', swatch: Colors.red),
-    ThemeOption(name: 'Azul', swatch: Colors.blue),
-    ThemeOption(name: 'Verde', swatch: Colors.green),
-    ThemeOption(name: 'Laranja', swatch: Colors.orange),
-  ];
+  "Duvel - Pilsner - 82 ibu",
+];
 
-  MyAppBar({super.key})
-      : super(
-          title: const Text("Bebidas"),
-          actions: [
-            PopupMenuButton<ThemeOption>(
-              icon: const Icon(Icons.more_vert),
-              onSelected: (ThemeOption choice) {
-                print('Selecionada a cor: ${choice.name}');
-                // A lógica de trocar o tema entrará aqui futuramente.
-              },
-              itemBuilder: (BuildContext context) {
-                return menuCores.map((ThemeOption choice) {
-                  return PopupMenuItem<ThemeOption>(
-                    value: choice,
-                    child: Text(choice.name),
-                  );
-                }).toList();
-              },
-            ),
-          ],
-        );
+void main() {
+  MyApp app = MyApp();
+
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
@@ -47,29 +19,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.deepPurple),
+
       debugShowCheckedModeBanner: false,
+
       home: Scaffold(
-        // Chamando nossa AppBar modularizada
-        appBar: MyAppBar(),
-        body: DataBodyWidget(objects: const [
-          "La Fin Du Monde - Bock - 65 ibu",
-          "Sapporo Premiume - Sour Ale - 54 ibu",
-          "Duvel - Pilsner - 82 ibu"
-        ]),
-        bottomNavigationBar: NewNavBar(icones: const [
-          Icons.coffee_outlined,
-          Icons.local_drink_outlined,
-          Icons.flag_outlined,
-        ]),
+        appBar: AppBar(title: const Text("Dicas")),
+
+        body: DataBodyWidget(objects: dataObjects),
+
+        bottomNavigationBar: NewNavBar(),
       ),
     );
   }
 }
 
 class NewNavBar extends StatelessWidget {
-  final List<IconData> icones;
-
-  NewNavBar({this.icones = const []});
+  NewNavBar();
 
   void botaoFoiTocado(int index) {
     print("Tocaram no botão $index");
@@ -77,41 +42,44 @@ class NewNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<BottomNavigationBarItem> itensDaBarra = icones
-        .map(
-          (icone) => BottomNavigationBarItem(
-            label: "Item", // Label genérico por enquanto
-            icon: Icon(icone),
-          ),
-        )
-        .toList();
-
     return BottomNavigationBar(
       onTap: botaoFoiTocado,
-      items: itensDaBarra,
+      items: const [
+        BottomNavigationBarItem(
+          label: "Cafés",
+
+          icon: Icon(Icons.coffee_outlined),
+        ),
+
+        BottomNavigationBarItem(
+          label: "Cervejas",
+          icon: Icon(Icons.local_drink_outlined),
+        ),
+
+        BottomNavigationBarItem(
+          label: "Nações",
+          icon: Icon(Icons.flag_outlined),
+        ),
+      ],
     );
   }
 }
 
 class DataBodyWidget extends StatelessWidget {
-  final List<String> objects;
+  List<String> objects;
 
   DataBodyWidget({this.objects = const []});
 
+  Expanded processarUmElemento(String obj) {
+    return Expanded(child: Center(child: Text(obj)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Expanded> allTheLines = objects
-        .map(
-          (obj) => Expanded(
-            child: Center(child: Text(obj)),
-          ),
-        )
-        .toList();
-
-    return Column(children: allTheLines);
+    return Column(
+      children: objects
+          .map((obj) => Expanded(child: Center(child: Text(obj))))
+          .toList(),
+    );
   }
-}
-
-void main() {
-  runApp(MyApp());
 }
